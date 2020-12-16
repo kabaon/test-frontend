86e3a31c1aebe7592e05690b17d85eb71173a43b
kabaon <75877001+kabaon@users.noreply.github.com>
2020-12-16 12:26:34 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 alert("はろー、ぎっとはぶ");
+
+let ipt = document.querySelector
+('input');
+
+let btn = document.querySelector
+('button');
+
+let elem = document.querySelector('p');
+btn.addEventListener('click', () => {
+  elem.innerText = ipt.value;
+} );
